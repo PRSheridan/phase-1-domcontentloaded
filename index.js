ce0e1bc4f7c633004e22b97ce1a055f9d07be2bc
Phil <philrsheridan@gmail.com>
2023-10-10 15:21:21 -0400

    passed 1 test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('DOM LOADED');
+    const ptext = document.getElementById("text")
+    ptext.textContent = "This is really cool!"
+}) 
